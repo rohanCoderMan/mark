fe8210a006ef960384062c09c856e82003389ed8
Rohan <84845548+rohanCoderMan@users.noreply.github.com>
2022-02-18 08:54:33 +0530

    Add files via upload

---
+++ setup.bat
@@ -0,0 +1,35 @@
+@echo off
+echo Installing pyttsx3
+python -m pip install pyttsx3 --user
+echo Installing SpeechRecognition 
+python -m  pip install SpeechRecognition --user
+echo Installing pyaudio
+python -m pip install pyaudio --user
+echo Installing flask
+python -m pip install flask --user
+echo Installing pywhatkit
+python -m pip install pywhatkit --user
+echo Installing wikipidea
+python -m pip install wikipidea --user
+echo Installing pyautogui
+python -m pip install pyautogui --user
+echo Installing os
+python -m pip install os --user
+echo Installing pyjokes
+python -m pip install pyjokes --user
+echo Installing pydictionary
+python -m pip install pydictionary --user
+echo Installing date time
+python -m pip install datetime --user
+echo Installing play sound
+python -m pip install playsound --user
+echo Installing pywin32
+python -m pip install pywin32 --user
+echo Installing pywikihow
+python -m pip install pywikihow --user
+echo Installing requests
+python -m pip install requests --user
+echo Installing bs4
+python -m pip install bs4 --user
+
+echo -------------------------------DONE-----------------------------
